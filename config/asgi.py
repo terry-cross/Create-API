b90392cc7bc613d069ac10c28cb2ba18d0417f37
Terry Cross <tcrossband.86@gmail.com>
2021-10-01 16:25:31 -0700

    drf-apis-nutshell-asgi-mess-terry-cross

--- config/asgi.py
+++ config/asgi.py
@@ -14,3 +14,15 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_asgi_application()
+
+"""
+The early life of one Jalal Belsifar
+
+was frought with wild adventures 
+
+as a boy who lived his life growing up 
+
+on the African Savanna, conferring with
+
+birds and wrestling crocodiles.
+"""
